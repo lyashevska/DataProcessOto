61bf9659aaaf1e95cf71396e6c1c071cfed2715b
Olga Lyashevska <o.lyashevskaya@gmail.com>
2018-03-19 10:40:42 +0000

    done

--- processing.py
+++ processing.py
@@ -1,3 +1,38 @@
-f = open('data/0.8x 1-43 images done.txt')
-content = f.read()
-f.close()
+import re
+import os
+from glob import glob
+import csv
+
+# change dir
+os.chdir('/home/olga/Documents/DataProcessOto/data')
+
+filenames = glob('*.txt')
+
+results = []
+for i in range(len(filenames)):
+    f = open(filenames[i], 'r')
+    lines = f.readlines()
+    #f.close()
+    res = []
+    for line in lines:
+        # line containing tif
+        m1 = re.search("^.*tif.*$", line)
+        if m1:
+            #print(m1.group)
+            fishid = m1.group().split(".tif")[0]
+
+        # line starting with numeric
+        m2 = re.search("^\d.*", line)
+        if m2:
+            #print(m2.group())
+            z = fishid + '\t' + m2.group()
+            print(z)
+            res += [z]
+    results+=res
+
+# write to a file
+with open("../output.csv", "w") as f:
+    for item in results:
+        f.write(item)
+        f.write("\n")
+
